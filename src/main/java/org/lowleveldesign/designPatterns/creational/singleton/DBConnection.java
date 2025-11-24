8c23641216c95b48dc71c5f4d205aa3d5d838118
JDevRohanY <rohanyadavudp10@gmail.com>
2025-11-24 23:11:35 +0530

    Implementing thread safe function for multi threaded env

--- src/main/java/org/lowleveldesign/designPatterns/creational/singleton/DBConnection.java
+++ src/main/java/org/lowleveldesign/designPatterns/creational/singleton/DBConnection.java
@@ -1,7 +1,12 @@
 package org.lowleveldesign.designPatterns.creational.singleton;
 
 public class DBConnection {
-    private static DBConnection instance = new DBConnection();
+    /* But this will increase the loading time of the application due to eager loading as this object
+       Is initialised at compile time, and we cannot pass variable in constructor
+
+    private static DBConnection instance = new DBConnection(); */
+
+    private static DBConnection instance = null;
     String url;
     String username;
     String password;
@@ -11,11 +16,21 @@ public class DBConnection {
 
     }
 
-    public static DBConnection getInstance(){
+    /* public static DBConnection getInstance(){
         //This is critical section which is not thread safe in multithreading environment
-        /*if(instance == null){
+        if(instance == null){
             instance = new DBConnection();
-        }*/
+        }
+        return instance;
+    } */
+
+    public synchronized static DBConnection getInstance(){
+        //But this result in slow performance as thread need to wait,
+        // also this is termed as lazy loading, and once created still thread need to wait
+        // as this is synchronised function block
+        if(instance == null){
+            instance = new DBConnection();
+        }
         return instance;
     }
 }
